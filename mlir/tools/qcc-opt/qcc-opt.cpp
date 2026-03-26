4b72aafe0535dabb55621c087d22fe779fbb64cb
Julian Farnsteiner <j.farnsteiner@parityqc.com>
2026-03-26 17:26:43 +0000

    blank conversion pass

--- mlir/tools/qcc-opt/qcc-opt.cpp
+++ mlir/tools/qcc-opt/qcc-opt.cpp
@@ -4,10 +4,12 @@
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "qcc/Conversion/JaspToQC/JaspToQC.h"
 #include "qcc/Dialect/Jasp/IR/Jasp.h"
 
 int main(int argc, char** argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::func::FuncDialect, mlir::arith::ArithDialect, jasp::JaspDialect, mlir::qc::QCDialect>();
+  qcc::registerJaspToQC();
   return mlir::asMainReturnCode(mlir::MlirOptMain(argc, argv, "qcc optimizer", registry));
 }
