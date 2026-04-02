1987e0fa2eb48775cf5626e7ed159119f0988a5c
Julian Farnsteiner <j.farnsteiner@parityqc.com>
2026-04-02 06:57:01 +0000

    pin commit hash for stablehlo

--- mlir/cmake/LateExternalDependencies.cmake
+++ mlir/cmake/LateExternalDependencies.cmake
@@ -4,7 +4,7 @@ set(FETCH_PACKAGES "")
 FetchContent_Declare(
   stablehlo
   GIT_REPOSITORY https://github.com/openxla/stablehlo.git
-  GIT_TAG v1.13.8)
+  GIT_TAG bdbe31e8a1a2f4884c29c1c685de36e74ba6a68d) # v1.13.8 - latest compatible with LLVM 22.1.0
 set(STABLEHLO_BUILD_EMBEDDED
     ON
     CACHE BOOL "Build StableHLO as part of another project")
