a52614fce4f31de63099c23d032b86934c9365c1
Reinhard Stahn <51020828+rainij@users.noreply.github.com>
2026-04-04 18:06:57 +0000

    wip: generate some LLVM IR

--- mlir/lib/Conversion/QCToQIRAdaptive/QCToQIRAdaptive.cpp
+++ mlir/lib/Conversion/QCToQIRAdaptive/QCToQIRAdaptive.cpp
@@ -1,6 +1,9 @@
 #include "qcc/Conversion/QCToQIRAdaptive/QCToQIRAdaptive.h"
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
+#include "mlir/IR/Builders.h"
 
 #include <llvm/Support/raw_ostream.h>
 
@@ -14,7 +17,29 @@ struct QCToQIRAdaptive final : impl::QCToQIRAdaptiveBase<QCToQIRAdaptive> {
 
 protected:
   void runOnOperation() override {
-    // FIXME: implement this!
+    mlir::func::FuncOp funcOp = getOperation();
+    mlir::OpBuilder builder(funcOp->getContext());
+
+    // FIXME: finish implementation
+
+    funcOp.eraseBody();
+
+    mlir::Block* entryBlock = funcOp.addEntryBlock();
+    mlir::Block* exitBlock = funcOp.addBlock();
+
+    builder.setInsertionPointToEnd(entryBlock);
+    builder.create<mlir::LLVM::BrOp>(funcOp.getLoc(), mlir::ValueRange{},
+                                     exitBlock); // FIXME: builder.create is deprecated
+
+    builder.setInsertionPointToEnd(exitBlock);
+    if (funcOp.getNumResults() > 0) {
+      // FIXME: make this robust
+      mlir::Type retType = funcOp.getResultTypes()[0];
+      mlir::Value zeroVal = builder.create<mlir::LLVM::ZeroOp>(funcOp.getLoc(), retType);
+      builder.create<mlir::func::ReturnOp>(funcOp.getLoc(), zeroVal);
+    } else {
+      builder.create<mlir::func::ReturnOp>(funcOp.getLoc());
+    }
   }
 };
 
--- mlir/test/tools/qcc-opt/qc-to-qir-adaptive.mlir
+++ mlir/test/tools/qcc-opt/qc-to-qir-adaptive.mlir
@@ -1,8 +1,16 @@
 // RUN: qcc-opt %s -qc-to-qir-adaptive | FileCheck %s
 
 func.func @test() {
-    // FIXME: add something here.
+    // FIXME: add better and more tests
+
+    %0 = qc.alloc : !qc.qubit
+    qc.h %0 : !qc.qubit
+    %m0 = qc.measure %0 : !qc.qubit -> i1
+    // recording
+
     return
 }
+
+func.func @other_func() { return }
 
 // CHECK-LABEL: @test
