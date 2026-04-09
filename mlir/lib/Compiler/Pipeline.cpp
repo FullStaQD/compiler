6f159111542dda2f305215132c4cede053156722
Reinhard Stahn <51020828+rainij@users.noreply.github.com>
2026-04-09 14:46:25 +0000

    Update qcc pipeline to use adaptive profile lowering

--- mlir/lib/Compiler/Pipeline.cpp
+++ mlir/lib/Compiler/Pipeline.cpp
@@ -1,8 +1,11 @@
 
 #include "qcc/Compiler/Pipeline.h"
 
+#include "qcc/Conversion/ToQIR/ToQIR.h"
+
 #include <llvm/Support/raw_ostream.h>
-#include <mlir/Conversion/QCToQIR/QCToQIR.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Pass/PassManager.h>
 #include <mlir/Transforms/Passes.h>
 
 namespace qcc {
@@ -12,7 +15,13 @@ void buildQuantumPipeline(mlir::PassManager& pm) {
   pm.addPass(mlir::createCanonicalizerPass());
 
   // QC → QIR Conversion
-  pm.addPass(mlir::createQCToQIR());
+  pm.addPass(qcc::createToQIRPrep());
+
+  mlir::OpPassManager& fpm = pm.nest<mlir::func::FuncOp>();
+  fpm.addPass(qcc::createQCToQIR());
+  fpm.addPass(qcc::createStdToQIR());
+
+  pm.addPass(qcc::createToQIRFinalize());
 
   // Cleanup QIR
   pm.addPass(mlir::createCanonicalizerPass());
