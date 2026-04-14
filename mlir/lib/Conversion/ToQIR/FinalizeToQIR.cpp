d948959560a78b536375587d98e9fa3743dfd055
Reinhard Stahn <51020828+rainij@users.noreply.github.com>
2026-04-14 08:50:25 +0000

    Refactor finalize to QIR pass

--- mlir/lib/Conversion/ToQIR/FinalizeToQIR.cpp
+++ mlir/lib/Conversion/ToQIR/FinalizeToQIR.cpp
@@ -5,13 +5,11 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/LLVMIR/LLVMTypes.h"
-#include "mlir/Dialect/QC/IR/QCDialect.h" // FIXME: remove
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "qcc/Conversion/ToQIR/Constants.h"
 #include "qcc/Conversion/ToQIR/ToQIR.h"
 
-#include <llvm/Support/raw_ostream.h>
 #include <mlir/IR/PatternMatch.h>
 #include <mlir/Transforms/GreedyPatternRewriteDriver.h>
 
@@ -19,20 +17,14 @@ using namespace mlir;
 
 namespace {
 
-struct PrepareFuncAttrs : public OpRewritePattern<func::FuncOp> {
+struct CleanupFuncAttrs : public OpRewritePattern<func::FuncOp> {
   using OpRewritePattern<func::FuncOp>::OpRewritePattern;
 
   LogicalResult matchAndRewrite(func::FuncOp funcOp, PatternRewriter& /*rewriter*/) const override {
     OpBuilder builder(funcOp->getContext());
 
     if (funcOp->hasAttr(qcc::entryPointAttrName)) {
-      llvm::errs() << "!!! found entry point: " << funcOp.getName() << "\n";
       funcOp->removeAttr(qcc::entryPointAttrName);
-      // FIXME:
-      // funcOp->setAttr("passthrough", builder.getArrayAttr({builder.getStringAttr("entry_point")}));
-    } else {
-      /// FIXME: requires capability (6)
-      llvm::errs() << "!!! found IR defined function: " << funcOp.getName() << "\n";
     }
 
     return failure();
@@ -51,32 +43,27 @@ struct FinalizeToQIR final : public impl::FinalizeToQIRBase<FinalizeToQIR> {
 
 protected:
   void runOnOperation() override {
-    // FIXME: finish impl
-
     ModuleOp moduleOp = getOperation();
     auto* ctx = moduleOp.getContext();
 
-    // Prepare func attrs.
+    // Cleanup func attrs.
     {
       RewritePatternSet patterns(ctx);
-      patterns.add<PrepareFuncAttrs>(ctx);
+      patterns.add<CleanupFuncAttrs>(ctx);
 
       if (failed(applyPatternsGreedily(moduleOp, std::move(patterns)))) {
         signalPassFailure();
       }
     }
 
-    // Full conversion to LLVM.
+    // Finish conversion to LLVM.
     {
       LLVMConversionTarget target(*ctx);
       target.addLegalOp<ModuleOp>();
 
-      target.addLegalDialect<qc::QCDialect>(); // FIXME: remove
-
       LLVMTypeConverter typeConverter(ctx);
       RewritePatternSet patterns(ctx);
 
-      arith::populateArithToLLVMConversionPatterns(typeConverter, patterns); // FIXME: maybe not needed here
       populateFuncToLLVMConversionPatterns(typeConverter, patterns);
 
       if (failed(applyFullConversion(moduleOp, target, std::move(patterns)))) {
--- mlir/test/tools/qcc-opt/ToQIR/finalize-to-qir.mlir
+++ mlir/test/tools/qcc-opt/ToQIR/finalize-to-qir.mlir
@@ -1,12 +1,21 @@
 // RUN: qcc-opt %s -finalize-to-qir | FileCheck %s
 
-func.func @test() -> i64 attributes { qcc.entry_point } {
+func.func @main() -> i64 attributes { qcc.entry_point, passthrough = [ "some_attr" ] } {
+    %0 = call @get_zero() : () -> i64
+    return %0 : i64
+}
+
+func.func @get_zero() -> i64 {
     %0 = llvm.mlir.constant(0 : i64) : i64
     return %0 : i64
 }
 
-// FIXME: update checks
-// CHECK-LABEL:   llvm.func
+// CHECK-LABEL:   llvm.func @main() -> i64 attributes {passthrough = ["some_attr"]} {
+// CHECK:           %[[CALL_0:.*]] = llvm.call @get_zero() : () -> i64
+// CHECK:           llvm.return %[[CALL_0]] : i64
+// CHECK:         }
+
+// CHECK-LABEL:   llvm.func @get_zero() -> i64 {
 // CHECK:           %[[MLIR_0:.*]] = llvm.mlir.constant(0 : i64) : i64
 // CHECK:           llvm.return %[[MLIR_0]] : i64
 // CHECK:         }
