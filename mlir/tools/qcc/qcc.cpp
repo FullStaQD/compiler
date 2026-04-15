23a0f9d98d94093abd02eff59f8fb3595f71c307
Julian Farnsteiner <143944161+julfarn@users.noreply.github.com>
2026-04-15 10:11:54 +0200

    Qrisp frontend: Assembly of classical pipeline. (#31)
    This completes, for now, the qrisp integration according to #10 from the
    FullStaQD side.
    
    - Removed `stablehlo` dependency, as we can rely on qrisp to lower to
    `linalg` on the classical side.
    - Added conversion of Rank-0-Tensors like `tensor<i64>` to plain values
    like `i64` in `JaspToQC` pass.
    - Removed appearance of `!jasp.QuantumState` type that remained in
    function signatures after `JaspToQC` pass.
    - Assembled pipeline to lower classical operations to `memref`, `arith`,
    and `scf` dialects.
    
    ---------
    
    Signed-off-by: Julian Farnsteiner <143944161+julfarn@users.noreply.github.com>
    Co-authored-by: Julian Farnsteiner <j.farnsteiner@parityqc.com>
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>
    Co-authored-by: Reinhard Stahn <51020828+rainij@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- mlir/tools/qcc/qcc.cpp
+++ mlir/tools/qcc/qcc.cpp
@@ -1,17 +1,35 @@
+#include "mlir/Conversion/VectorToSCF/VectorToSCF.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Arith/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Bufferization/Transforms/FuncBufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Func/Extensions/InlinerExtension.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/MemRef/Transforms/AllocationOpInterfaceImpl.h"
+#include "mlir/Dialect/MemRef/Transforms/Passes.h"
+#include "mlir/Dialect/QC/IR/QCDialect.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/SCF/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Dialect/Tensor/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/IR/Diagnostics.h"
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "qcc/Compiler/Pipeline.h"
+#include "qcc/Dialect/Jasp/IR/Jasp.h"
 
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/ToolOutputFile.h>
-#include <mlir/Dialect/Arith/IR/Arith.h>
-#include <mlir/Dialect/Func/IR/FuncOps.h>
-#include <mlir/Dialect/QC/IR/QCDialect.h>
-#include <mlir/IR/Diagnostics.h>
-#include <mlir/InitAllDialects.h>
-#include <mlir/Parser/Parser.h>
-#include <mlir/Pass/PassManager.h>
-#include <mlir/Support/FileUtilities.h>
-#include <mlir/Tools/mlir-opt/MlirOptMain.h>
+#include <llvm/Support/raw_ostream.h>
 
 namespace cl = llvm::cl;
 
@@ -25,7 +43,18 @@ int main(int argc, char** argv) {
   cl::ParseCommandLineOptions(argc, argv, "qcc - quantum compiler collection\n");
 
   mlir::DialectRegistry registry;
-  registry.insert<mlir::func::FuncDialect, mlir::arith::ArithDialect, mlir::qc::QCDialect>();
+  registry.insert<mlir::func::FuncDialect, mlir::arith::ArithDialect, mlir::tensor::TensorDialect,
+                  mlir::bufferization::BufferizationDialect, mlir::linalg::LinalgDialect, mlir::scf::SCFDialect,
+                  jasp::JaspDialect, mlir::qc::QCDialect>();
+
+  // Extension registration
+  mlir::arith::registerBufferizableOpInterfaceExternalModels(registry);
+  mlir::linalg::registerBufferizableOpInterfaceExternalModels(registry);
+  mlir::scf::registerBufferizableOpInterfaceExternalModels(registry);
+  mlir::tensor::registerBufferizableOpInterfaceExternalModels(registry);
+  mlir::bufferization::func_ext::registerBufferizableOpInterfaceExternalModels(registry);
+  mlir::memref::registerAllocationOpInterfaceExternalModels(registry);
+  mlir::func::registerInlinerExtension(registry);
 
   mlir::MLIRContext context(registry);
 
