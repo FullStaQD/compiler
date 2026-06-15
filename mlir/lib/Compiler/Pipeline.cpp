dc301c578db223f468b124357e41a733fe7de9ba
Enrique <e.bejarano@parityqc.com>
2026-06-15 11:30:22 +0000

    Prepare to intrinsics pass.

--- mlir/lib/Compiler/Pipeline.cpp
+++ mlir/lib/Compiler/Pipeline.cpp
@@ -95,6 +95,9 @@ void buildQuantumPipeline(mlir::PassManager& pm) {
   pm.addPass(mlir::createCanonicalizerPass());
   pm.addPass(mlir::createCSEPass());
   pm.addPass(mlir::createRemoveDeadValuesPass());
+
+  // conversion from LLVM QIR to LLVM with intrinsics to lower to HiSEP-Q assembly.
+  pm.addPass(qcc::createConvertToIntrinsics());
 }
 
 } // namespace qcc
