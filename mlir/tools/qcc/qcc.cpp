b2f296b34ea54370775d9609d3cd49ac0c4cd4fd
Reinhard Stahn <rainij36@proton.me>
2026-06-23 14:37:10 +0000

    Fix include order

--- mlir/tools/qcc/qcc.cpp
+++ mlir/tools/qcc/qcc.cpp
@@ -35,15 +35,16 @@
 #include "mlir/Target/LLVMIR/Export.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/SystemUtils.h"
+#include "llvm/Support/ToolOutputFile.h"
+
 #include <cstdint>
-#include <llvm/Bitcode/BitcodeWriter.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/CommandLine.h>
-#include <llvm/Support/ErrorHandling.h>
-#include <llvm/Support/SourceMgr.h>
-#include <llvm/Support/SystemUtils.h>
-#include <llvm/Support/ToolOutputFile.h>
 
 namespace cl = llvm::cl;
 
