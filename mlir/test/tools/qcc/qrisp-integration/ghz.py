0216b087b36b26bf9a07710f265d0a4e6247d407
Reinhard Stahn <rainij36@proton.me>
2026-06-24 09:25:59 +0200

    Relax license header checks for test folder (#94)
    Resolves #89.

--- mlir/test/tools/qcc/qrisp-integration/ghz.py
+++ mlir/test/tools/qcc/qrisp-integration/ghz.py
@@ -1,12 +1,3 @@
-#  ===----------------------------------------------------------------------===//
-
-#  Part of the FullStaQD Project, under the Apache License v2.0 with LLVM
-#  Exceptions.
-#  See <repo-root>/LICENSE.txt for license information.
-#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#  ===----------------------------------------------------------------------===//
-
 from qrisp import QuantumVariable, h, cx, measure, q_fori_loop
 
 def prepare_ghz():
